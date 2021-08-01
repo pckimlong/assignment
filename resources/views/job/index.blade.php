5222aeb0cc6518f62e257c165312941587f41050
pckimlong <pochkimlong88@gmail.com>
2021-08-01 16:59:29 +0700

    before create job list page

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.job')
 @section('content')
 <section class="job-section">
-  <app-component />
+  {{-- <app-component /> --}}
+  <app-component></app-component>
 </section>  
 @endsection
